0cfec99012e530bbac46a910fc4d543089b2af15
Muxammed Layicov <layicovmuxammed@gmail.com>
2024-09-09 21:29:03 +0400

    feat: getClerkUsers for get users

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,16 +2,22 @@
 
 import { ReactNode } from "react";
 import {
-    LiveblocksProvider,
-    ClientSideSuspense,
-  } from "@liveblocks/react/suspense"
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense"
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.action";
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
-        <ClientSideSuspense fallback={<Loader/>}>
-          {children}
-        </ClientSideSuspense>
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds })
+        return users
+      }}>
+      <ClientSideSuspense fallback={<Loader />}>
+        {children}
+      </ClientSideSuspense>
     </LiveblocksProvider>
   )
 }
