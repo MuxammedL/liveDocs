f50c951289355f97f733efab3e9bf1f0d0620ad8
Muxammed Layicov <layicovmuxammed@gmail.com>
2024-09-09 21:32:10 +0400

    feat: added it here

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,25 +1,29 @@
-
+"use client"
 import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense"
 import Loader from "./Loader"
 import Header from "./Header"
 import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs"
 import { Editor } from "./editor/Editor"
+import ActiveCollaborators from "./ActiveCollaborators"
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({roomId,roomMetadata}:CollaborativeRoomProps) => {
     return (
-        <RoomProvider id="my-room">
+        <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<Loader />}>
                 <div className="collaborative-room">
                     <Header>
                         <div className="flex w-fit items-center justify-center gap-2">
                             <p className="document-title">Share</p>
                         </div>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
+                        <div className="flex w-full flex-1 justify-end gap-2">
+                            <ActiveCollaborators />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
                     </Header>
                     <Editor />
                 </div>
